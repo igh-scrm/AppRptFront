f5eb511e4975618df35b67b70d477dba21afa3e3
Marina <aye228gash@mail.ru>
2023-05-08 22:03:38 +0300

    Добавлена страница авторизации, настроена навигация на фреймах

--- RotFrontApplication/MainWindow.xaml.cs
+++ RotFrontApplication/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RotFrontApplication.HelperClass;
+using RotFrontApplication.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,8 @@ namespace RotFrontApplication
         public MainWindow()
         {
             InitializeComponent();
+            NavigateClass.frmNav = FrmMain;
+            FrmMain.Navigate(new AuthPage());
         }
     }
 }
--- RotFrontApplication/obj/Debug/RotFrontApplication_Content.g.i.cs
+++ RotFrontApplication/obj/Debug/RotFrontApplication_Content.g.i.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("models/modelrotfront.context.tt")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("models/modelrotfront.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("helperclass/model1.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("helperclass/model1.tt")]
 
 
